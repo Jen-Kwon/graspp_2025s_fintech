3ddcac378e54f4a4c4e64c368e960ac6a8e5dec1
snakada1615 <khf04440@nifty.com>
2025-04-29 16:14:00 +0900

    update files

--- script/functionsNakada.py
+++ script/functionsNakada.py
@@ -221,3 +221,60 @@ def getTableFromPDF(sourceNum, pages):
             res[i]=df
 
         return res
+
+def importWB(database_id, indicator_id, year_from, year_to):
+    import requests
+    import pandas as pd
+
+    url = (
+        f"https://data360api.worldbank.org/data360/data"
+        f"?DATABASE_ID={database_id}&INDICATOR={indicator_id}"
+        f"&timePeriodFrom={year_from}&timePeriodTo={year_to}&skip=0"
+    )
+    headers = {"accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # The data is under the "value" key
+    if "value" in data and data["value"]:
+        df = pd.DataFrame(data["value"])
+        return df
+    else:
+        print("No data available for the requested indicator and database.")
+        return False
+
+# example of function usage
+# mydf_ICTPolicy = importWB("ITU_ICT", "ITU_ICT_G5_DIG_ECON", "2000", "2025")
+# mydf_FarmCredit = importWB("FAO_IC", "FAO_IC_23068", "2000", "2025")
+# mydf_Value_AgProduction = importWB("FAO_MK", "FAO_MK_22016", "2000", "2025")
+# mydf_Value_AgProcessing = importWB("FAO_MK", "FAO_MK_22077", "2000", "2025")
+# mydf_Fertilizer = importWB("WB_WDI", "WB_WDI_AG_CON_FERT_ZS", "2000", "2025")
+# print(mydf_ICTPolicy.head())
+
+def filterWB(df, countries, colNames, newColNames):
+    filtered_df = df[df['REF_AREA'].isin(countries)][colNames]
+    filtered_df.columns = newColNames
+    return filtered_df
+
+# asianCountries = ['AFG','ARM','AZE','BHR','BGD','BTN','BRN','KHM',
+#                   'CHN','GEO','IND','IDN','IRN','IRQ','ISR','JPN','JOR','KAZ','PRK',
+#                   'KOR','KWT','KGZ','LAO','LBN','MYS','MDV','MNG','MMR','NPL','OMN',
+#                   'PAK','PSE','PHL','QAT','SAU','SGP','LKA','SYR','TJK','THA','TLS',
+#                   'TUR','TKM','ARE','UZB','VNM','YEM']
+
+# mydf_ICTPolicy_filtered = filterDF(mydf_ICTPolicy, asianCountries, ["REF_AREA", "TIME_PERIOD", "OBS_VALUE"], ["areaCode", "year", "ICTPolicy"])
+
+def setIndexWB(df, key):
+    return df.set_index(key)
+
+# mydf_ICTPolicy_indexed = setIndex(mydf_ICTPolicy_filtered, ["areaCode", "year"])
+# mydf_FarmCredit_indexed = setIndex(mydf_FarmCredit_filtered, ["areaCode", "year"])
+# mydf_Value_AgProduction_indexed = setIndex(mydf_Value_AgProduction_filtered, ["areaCode", "year"])
+# mydf_Value_AgProcessing_indexed = setIndex(mydf_Value_AgProcessing_filtered, ["areaCode", "year"])
+# mydf_Fertilizer_indexed = setIndex(mydf_Fertilizer_filtered, ["areaCode", "year"])
+
+def exportCSV(df, fileName):
+    output = 'data_processed/' + fileName + '.csv'
+    df.to_csv(output)
+    return True
